8069e3989ff1716b8cd99f7f51256008f7b867a8
Ekaterina Savostina <74173325+sovetskyk@users.noreply.github.com>
2022-12-12 16:14:27 +0100

    Create odd_function.py

---
+++ odd_function.py
@@ -0,0 +1,24 @@
+"""
+x = int(input("What's x? "))
+
+if x % 2 == 0:
+    print("Even")
+
+else:
+    print("Odd")
+"""
+
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print ("Even")
+    else:
+        print ("Odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+main()
